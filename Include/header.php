8c8b3c248c87f7b3c32edb3f229e002168ba6011
WalidBOULFRAD <walid.boulfrad@gmail.com>
2016-03-17 11:52:14 +0100

    Finalisation de l'entete du site

--- Include/header.php
+++ Include/header.php
@@ -12,6 +12,7 @@
 		<!--[if lte IE 8]><script src="../assets/js/ie/html5shiv.js"></script><![endif]-->
 		<link rel="stylesheet" href="../assets/css/main.css" />
 		<!--[if lte IE 9]><link rel="stylesheet" href="../assets/css/ie9.css" /><![endif]-->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 	</head>
 	<body>
 		<div id="page-wrapper">
@@ -23,18 +24,17 @@
 							<div class="12u">
 
 								<!-- Logo -->
-									<h1><a href="#" id="logo">Dev_IT</a></h1>
+									<h1><a href="../index.php" id="logo">Dev_IT</a></h1>
 
 								<!-- Nav -->
 									<nav id="nav">
-										<a href="../index.html">Accueil</a>
 										<a href="new.html">Actualités</a>
 										<a href="update.html">Mise à jours</a>
 										<a href="functionnalities.html">Fonctionnalités</a>
 										<a href="software.html">Logiciels</a>
 										<a href="porability.html">Portabilité</a>
 										<a href="accesories.html">Accessoires</a>
-										<a href="search.html">Recherche</a>
+										<a href="search.html"><i class="fa fa-search"></i></a>
 									</nav>
 
 							</div>
