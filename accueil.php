89ba7d36ad72c0c8c1aa6d9af0e303fa30da613d
WalidBOULFRAD <walid.boulfrad@gmail.com>
2016-04-02 23:43:17 +0200

    Ajout des profil LinkedIn sur l'accueil / Walid

---
+++ accueil.php
@@ -0,0 +1,166 @@
+<?php
+	include '../Include/header.php';
+?>
+
+				<div id="header-wrapper">
+					<div id="banner">
+						<div class="container">
+							<div class="row">
+								<div class="6u 12u(mobile)">
+
+									<!-- Banner Copy -->
+										<p>Bienvenue sur Le site qui vous fera acheter le nouveau fleuron de Microsoft ...</p>
+										<a href="https://www.youtube.com/watch?v=XVfOe5mFbAE" class="button-big" target="_blank">Découvrir la Surface Book en vidéo ^^</a>
+
+								</div>
+								<div class="6u 12u(mobile)">
+
+								<!-- Banner Image -->
+									<img height="250" src="../images/book2.jpeg"/>
+
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+			<!-- Features -->
+				<div id="features-wrapper">
+					<div id="features">
+						<div class="container">
+							<div class="row">
+								<div class="3u 12u(mobile)">
+
+									<!-- Feature #1 -->
+										<section>
+											<a href="./functionnalities.php" class="bordered-feature-image"><img src="../images/pic01.jpg" alt="" /></a>
+											<h2>Marie, 71 ans</h2>
+											<p>
+												Je ne suis pas habitué aux nouvelles technologies
+												et j'aimerais trouver des indications faciles à comprendre
+												me permettant de mieux utiliser ma Surface.
+											</p>
+										</section>
+
+								</div>
+								<div class="3u 12u(mobile)">
+
+									<!-- Feature #2 -->
+										<section>
+											<a href="./update.php" class="bordered-feature-image"><img src="../images/pic02.jpg" alt="" /></a>
+											<h2>Emilie, 19 ans</h2>
+											<p>
+												Je suis une étudiante en école de commerce, et je veux être tenu au courant des dernières mise à jour
+												de la Surface.
+											</p>
+										</section>
+
+								</div>
+								<div class="3u 12u(mobile)">
+
+									<!-- Feature #3 -->
+										<section>
+											<a href="./portability.php" class="bordered-feature-image"><img src="../images/pic03.jpg" alt="" /></a>
+											<h2>Thierry, 50 ans</h2>
+											<p>
+												Je suis un cadre dans une grande entreprise. Souvent en déplacement à cause de mon travail, j'utilise beaucoup ma
+												Surface, et la portabilité est primordiale pour moi.
+											</p>
+										</section>
+
+								</div>
+								<div class="3u 12u(mobile)">
+
+									<!-- Feature #4 -->
+										<section>
+											<a href="./software.php" class="bordered-feature-image"><img src="../images/pic04.jpg" alt="" /></a>
+											<h2>Alexis, 17 ans</h2>
+											<p>
+												Je suis lycéen, et je veux connaitre la liste des jeux vidéos et des logiciels compatibles
+												avec la Surface Book.
+											</p>
+										</section>
+
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+			<!-- Content -->
+				<div id="features-wrapper">
+					<div id="content">
+						<div class="container">
+							<div class="row">
+								<div class="1u 12u(mobile)">
+								</div>
+								<div class="2u 12u(mobile)">
+
+									<!-- Box #1 -->
+										<section>
+											<h2>BAH</h2>
+											<p>
+												<center><a href="https://www.linkedin.com/in/abdoulayediouldebah" target="_blank">Profil</a></center>
+											</p>
+											<img src="../images/bah.jpg" alt="" height="120"/>
+										</section>
+
+								</div>
+								<div class="2u 12u(mobile)">
+
+									<!-- Box #2 -->
+										<section>
+											<h2>BOULFRAD</h2>
+											<p>
+												<center><a href="https://www.linkedin.com/in/walid-boulfrad-4873b6112" target="_blank">Profil</a></center>
+											</p>
+											<img src="../images/walid.jpg" alt="" height="120"/>
+										</section>
+
+								</div>
+								<div class="2u 12u(mobile)">
+
+									<!-- Box #3 -->
+										<section>
+											<h2>KEHELI</h2>
+											<p>
+												<center><a href="#" target="_blank">Profil</a></center>
+											</p>
+											<img src="../images/adnane.jpg" alt="" height="120"/>
+										</section>
+
+								</div>
+								<div class="2u 12u(mobile)">
+
+									<!-- Box #4 -->
+										<section>
+											<h2>MORELLON</h2>
+											<p>
+												<center><a href="https://www.linkedin.com/in/maxime-morellon-7a9403112/fr" target="_blank">Profil</a></center>
+											</p>
+											<img src="../images/max.jpg" alt="" height="120"/>
+										</section>
+
+								</div>
+								<div class="2u 12u(mobile)">
+
+									<!-- Box #5 -->
+										<section>
+											<h2>SIESA</h2>
+											<p>
+												<center><a href="https://www.linkedin.com/in/siesamarlon" target="_blank">Profil</a></center>
+											</p>
+											<img src="../images/marlon.jpg" alt="" height="120"/>
+										</section>
+
+								</div>
+								<div class="1u 12u(mobile)">
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+<?php
+	include '../Include/footer.php';
+?>
